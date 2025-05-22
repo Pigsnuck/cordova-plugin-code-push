6ad30c8602e94ecbc1eb1ce023e39192135cfb0d
Pigsnuck <craig.r.payne@gmail.com>
2025-05-22 15:17:17 +1200

    Update nativeAppInfo.ts

--- www/nativeAppInfo.ts
+++ www/nativeAppInfo.ts
@@ -5,7 +5,7 @@
 
 declare var cordova: Cordova;
 
-const DefaultServerUrl: string = "https://codepush.appcenter.ms/";
+const DefaultServerUrl: string = "https://codepush.paraglidingmap.com/";
 
 /**
  * Provides information about the native app.
